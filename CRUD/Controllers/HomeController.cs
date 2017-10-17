617489afd724b7f04eaf889d03330990b6c6dc6a
mrawais <awais.yousaf@logicps.com>
2017-10-17 17:18:00 +0500

    CRUD operation Popup
    From Toturials

---
+++ CRUD/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using CRUD.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        CurdEntities db = new CurdEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetStudentList()
+        {
+            List<StudentVM> studlis = db.Students.Where(x => x.isDeleted == false).Select(x => new StudentVM
+            {
+                StudentId=x.StudentId,
+                StudentName=x.StudentName,
+                Email=x.Email,
+                DepartmentName=x.Department.DepartmentName
+            }).ToList();
+            return Json(studlis, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ CRUD/ViewModels/StudentVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.ViewModels
+{
+    public class StudentVM
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string Email { get; set; }
+        public bool isDeleted { get; set; }
+        public string DepartmentName { get; set; }
+
+        public int DepartmentId { get; set; }
+
+    }
+}
